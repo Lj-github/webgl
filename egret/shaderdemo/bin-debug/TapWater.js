b4cb2eecf9a90d68d108b8243cd93defcd08818d
刘江 <1433727556@qq.com>
2018-08-31 20:45:01 +0800

    add glsl 文件

--- egret/shaderdemo/bin-debug/TapWater.js
+++ egret/shaderdemo/bin-debug/TapWater.js
@@ -17,42 +17,9 @@ var TapWater = (function (_super) {
     }
     TapWater.prototype.initUI = function () {
         //创建可点击的shader 点击水纹
-        var vertexSrc = "attribute vec2 aVertexPosition;\n" +
-            "attribute vec2 aTextureCoord;\n" +
-            "attribute vec2 aColor;\n" +
-            "uniform vec2 projectionVector;\n" +
-            "varying vec2 vTextureCoord;\n" +
-            "varying vec4 vColor;\n" +
-            "const vec2 center = vec2(-1.0, 1.0);\n" +
-            "void main(void) {\n" +
-            "   gl_Position = vec4( (aVertexPosition / projectionVector) + center , 0.0, 1.0);\n" +
-            "   vTextureCoord = aTextureCoord;\n" +
-            "   vColor = vec4(aColor.x, aColor.x, aColor.x, aColor.x);\n" +
-            "}";
-        var fragmentWater = [
-            "precision lowp float;\n" +
-                "varying vec2 vTextureCoord;",
-            "varying vec4 vColor;\n",
-            "uniform sampler2D uSampler;",
-            "uniform vec2 center;",
-            "uniform vec3 params;",
-            "uniform float time;",
-            "void main()",
-            "{",
-            "vec2 uv = vTextureCoord.xy;",
-            "vec2 texCoord = uv;",
-            "float dist = distance(uv, center);",
-            "if ( (dist <= (time + params.z)) && (dist >= (time - params.z)) )",
-            "{",
-            "float diff = (dist - time);",
-            "float powDiff = 1.0 - pow(abs(diff*params.x), params.y);",
-            "float diffTime = diff  * powDiff;",
-            "vec2 diffUV = normalize(uv - center);",
-            "texCoord = uv + (diffUV * diffTime);",
-            "}",
-            "gl_FragColor = texture2D(uSampler, texCoord);",
-            "}"
-        ].join("\n");
+        var vertexSrc = RES.getRes("vertex_glsl");
+        console.log(vertexSrc);
+        var fragmentWater = RES.getRes("fragmentWather_glsl");
         var waterFilter3 = new egret.CustomFilter(vertexSrc, fragmentWater, {
             center: { x: 0.5, y: 0.5 },
             params: { x: 10, y: 0.8, z: 0.1 },
--- egret/shaderdemo/src/TapWater.ts
+++ egret/shaderdemo/src/TapWater.ts
@@ -6,57 +6,12 @@ class TapWater extends eui.Component{
     }
     private initUI(){
         //创建可点击的shader 点击水纹
-         let vertexSrc =
-            "attribute vec2 aVertexPosition;\n" +
-            "attribute vec2 aTextureCoord;\n" +
-            "attribute vec2 aColor;\n" +
+        let vertexSrc = RES.getRes("vertex_glsl")
+        console.log(vertexSrc)
 
-            "uniform vec2 projectionVector;\n" +
+        let fragmentWater = RES.getRes("fragmentWather_glsl")
 
-            "varying vec2 vTextureCoord;\n" +
-            "varying vec4 vColor;\n" +
-
-            "const vec2 center = vec2(-1.0, 1.0);\n" +
-
-            "void main(void) {\n" +
-            "   gl_Position = vec4( (aVertexPosition / projectionVector) + center , 0.0, 1.0);\n" +
-            "   vTextureCoord = aTextureCoord;\n" +
-            "   vColor = vec4(aColor.x, aColor.x, aColor.x, aColor.x);\n" +
-            "}";
-
-
-          let fragmentWater = [  //水
-            "precision lowp float;\n" +
-            "varying vec2 vTextureCoord;",
-            "varying vec4 vColor;\n",
-            "uniform sampler2D uSampler;",
-
-            "uniform vec2 center;",
-            "uniform vec3 params;", // 10.0, 0.8, 0.1"
-            "uniform float time;",
-
-            "void main()",
-            "{",
-            "vec2 uv = vTextureCoord.xy;",
-            "vec2 texCoord = uv;",
-
-            "float dist = distance(uv, center);",
-
-            "if ( (dist <= (time + params.z)) && (dist >= (time - params.z)) )",
-            "{",
-            "float diff = (dist - time);",
-            "float powDiff = 1.0 - pow(abs(diff*params.x), params.y);",
-
-            "float diffTime = diff  * powDiff;",
-            "vec2 diffUV = normalize(uv - center);",
-            "texCoord = uv + (diffUV * diffTime);",
-            "}",
-
-            "gl_FragColor = texture2D(uSampler, texCoord);",
-            "}"
-        ].join("\n");
-
-         let waterFilter3 = new egret.CustomFilter(
+        let waterFilter3 = new egret.CustomFilter(
             vertexSrc,
             fragmentWater,
             {
@@ -80,13 +35,7 @@ class TapWater extends eui.Component{
             if (waterFilter3.uniforms.time > 1) {
                 waterFilter3.uniforms.time = 0.0;
             }
-
         }, this)
-
-
-
-
-
     }
     onTouch(){
         console.log("touch")
